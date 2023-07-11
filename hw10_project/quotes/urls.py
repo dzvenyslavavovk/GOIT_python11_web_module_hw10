909b3a9acb9cfc2c6cccd7eb121d3df380f3aa84
dzvenyslavavovk <dzvinavovk.25@gmail.com>
2023-07-12 01:28:59 +0300

    hw10

---
+++ hw10_project/quotes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "quotes"
+
+urlpatterns = [
+    path("", views.main, name="root"),
+    path("<int:page>", views.main, name="root_paginate"),
+    path("add_quote/", views.add_quote, name="add_quote"),
+    path("add_author/", views.add_author, name="add_author"),
+    path("author/<int:author_id>", views.author, name="author"),
+]
